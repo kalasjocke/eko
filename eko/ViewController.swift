ce255c3310dc91d5ce5e9aacf8304d3d42952d87
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-10-04 15:29:09 +0200

    Add ProximityService

--- eko/ViewController.swift
+++ eko/ViewController.swift
@@ -1,26 +1,42 @@
-//
-//  ViewController.swift
-//  eko
-//
-//  Created by Johannes Edelstam on 2014-10-04.
-//  Copyright (c) 2014 E+E. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
+    let proximityService = ProximityService()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        NSLog("hejhej");
+        
+        let action : Selector = "handleTap:"
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: action)
+        tapRecognizer.numberOfTapsRequired = 1
+        
+        self.view.addGestureRecognizer(tapRecognizer)
+    }
+    
+    func handleTap(sender: UITapGestureRecognizer) {
+        var alertController = UIAlertController(
+            title: "Start radio mode?",
+            message: nil,
+            preferredStyle: .Alert
+        )
+        alertController.addAction(UIAlertAction(
+            title: "No",
+            style: .Default,
+            handler: {(action: UIAlertAction!) in NSLog(":((")}
+        ))
+        alertController.addAction(UIAlertAction(
+            title: "Yes",
+            style: .Default,
+            handler: {(action: UIAlertAction!) in
+                NSLog("YES!!")
+                self.proximityService.startAdvertising()
+            }
+        ))
+        self.presentViewController(alertController, animated: true, completion: nil)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
