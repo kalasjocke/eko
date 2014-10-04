36e7986f8245ca6a8e5c89f090e6cd6c4203eed3
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-10-04 21:51:07 +0200

    Get rid of tap recognizer

--- eko/ViewController.swift
+++ eko/ViewController.swift
@@ -1,13 +1,3 @@
-//
-//  ViewController.swift
-//  eko
-//
-//  Created by Johannes Edelstam on 2014-10-04.
-//  Copyright (c) 2014 E+E. All rights reserved.
-//
-
-
-
 import UIKit
 import AudioToolbox
 import AVFoundation
@@ -28,16 +18,10 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let action : Selector = "handleTap:"
-        let tapRecognizer = UITapGestureRecognizer(target: self, action: action)
-        tapRecognizer.numberOfTapsRequired = 1
-        
-        self.view.addGestureRecognizer(tapRecognizer)
-    
         self.proximityService = ProximityService(delegate: self)
     }
     
-    func handleTap(sender: UITapGestureRecognizer) {
+    override func viewDidAppear(animated: Bool) {
         var alertController = UIAlertController(
             title: "Mode?",
             message: nil,
@@ -51,7 +35,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
                 
                 self.timer = NSTimer.scheduledTimerWithTimeInterval(60, target: self, selector: Selector("fetch"), userInfo: nil, repeats: true)
                 
-                self.proximityService.startReceiving()                
+                self.proximityService.startReceiving()
             }
         ))
         alertController.addAction(UIAlertAction(
