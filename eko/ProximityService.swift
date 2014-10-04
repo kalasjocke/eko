f4de3d6ae8064376f156894b727c9f71495f7847
Joakim Ekberg <jocke.ekberg@gmail.com>
2014-10-04 17:39:04 +0200

    Implement proximity detection

--- eko/ProximityService.swift
+++ eko/ProximityService.swift
@@ -1,33 +1,73 @@
 import Foundation
 import CoreLocation
 import CoreBluetooth
+import UIKit
 
-class ProximityService : NSObject, CBPeripheralManagerDelegate {
+class ProximityService : NSObject, CBPeripheralManagerDelegate, CLLocationManagerDelegate {
     let uuid : NSUUID = NSUUID(UUIDString: "B072B015-8E55-4AFD-B8EE-2DDFF1B16038")
     let identifier : NSString = "com.ekbergedelstam.myRegion"
 
     var peripheralManager : CBPeripheralManager!
-
-    func startAdvertising() {
-        self.peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+    var beaconRegion : CLBeaconRegion!
+    var beaconPeripheralData : NSDictionary!
+    var locationManager : CLLocationManager!
+    var delegate : ViewController;
+    
+    init(delegate: ViewController) {
+        self.beaconRegion = CLBeaconRegion(
+            proximityUUID: self.uuid,
+            identifier: self.identifier
+        )
+        self.delegate = delegate
     }
     
+    // Transmitting
+
+    func startTransmitting() {
+        NSLog("startTransmitting()")
+        
+        self.peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        self.beaconPeripheralData = self.beaconRegion.peripheralDataWithMeasuredPower(nil)
+    }
+
     func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
         if (peripheral.state == CBPeripheralManagerState.PoweredOn) {
-            let beaconRegion = CLBeaconRegion(
-                proximityUUID: self.uuid,
-                major: 1,
-                minor: 1,
-                identifier: self.identifier
-            )
-            
-            let beaconPeripheralData = beaconRegion.peripheralDataWithMeasuredPower(nil)
-            
             self.peripheralManager.startAdvertising(beaconPeripheralData)
         }
     }
     
     func peripheralManagerDidStartAdvertising(peripheral: CBPeripheralManager!, error: NSError!) {
         NSLog("peripheralManagerDidStartAdvertising()")
+    }
+    
+    // Receiving
+    
+    func startReceiving() {
+        NSLog("startReceiving()")
+        
+        self.locationManager = CLLocationManager()
+        self.locationManager.requestAlwaysAuthorization()
+        self.locationManager.pausesLocationUpdatesAutomatically = false
+        self.locationManager.delegate = self
+        self.locationManager.startMonitoringForRegion(self.beaconRegion)
+        
+        // Hack for monitoring straight away
+        self.locationManager.startRangingBeaconsInRegion(self.beaconRegion)
+        self.locationManager.startUpdatingLocation()
+    }
+    
+    func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        // self.locationManager.startRangingBeaconsInRegion(self.beaconRegion)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        // self.locationManager.stopMonitoringForRegion(self.beaconRegion)
+    }
+    
+    func locationManager(manager: CLLocationManager!, didRangeBeacons beacons: [AnyObject]!, inRegion region: CLBeaconRegion!) {
+        if beacons.count > 0 {
+            let nearestBeacon:CLBeacon = beacons[0] as CLBeacon
+            self.delegate.didUpdateProximity(nearestBeacon.rssi)
+        }
     }
 }
--- eko/ViewController.swift
+++ eko/ViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 
 class ViewController: UIViewController, UIGestureRecognizerDelegate {
-    let proximityService = ProximityService()
+    var proximityService : ProximityService!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -11,28 +11,31 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
         tapRecognizer.numberOfTapsRequired = 1
         
         self.view.addGestureRecognizer(tapRecognizer)
+    
+        self.proximityService = ProximityService(delegate: self)
     }
     
     func handleTap(sender: UITapGestureRecognizer) {
         var alertController = UIAlertController(
-            title: "Start radio mode?",
+            title: "Mode?",
             message: nil,
             preferredStyle: .Alert
         )
         alertController.addAction(UIAlertAction(
-            title: "No",
+            title: "Radio",
             style: .Default,
-            handler: {(action: UIAlertAction!) in NSLog(":((")}
+            handler: {(action: UIAlertAction!) in self.proximityService.startReceiving() }
         ))
         alertController.addAction(UIAlertAction(
-            title: "Yes",
+            title: "Person",
             style: .Default,
-            handler: {(action: UIAlertAction!) in
-                NSLog("YES!!")
-                self.proximityService.startAdvertising()
-            }
+            handler: {(action: UIAlertAction!) in self.proximityService.startTransmitting() }
         ))
         self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    func didUpdateProximity(proximity : Int) {
+        NSLog(proximity.description)
     }
 
     override func didReceiveMemoryWarning() {
